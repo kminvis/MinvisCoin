44448c2034181aa2fc8ed514858c429f1dbcabb8
kminvis <kminvis@gmail.com>
2022-11-27 13:13:08 +0900

    candle

--- public/Python/BTC_Chart.py
+++ public/Python/BTC_Chart.py
@@ -1,18 +1,31 @@
 import matplotlib.pyplot as plt
 from pyodide.http import open_url 
 import pandas as pd
+import time
 
-url = "https://api.upbit.com/v1/candles/minutes/1?market=KRW-BTC&count=2400"
+url = "https://api.upbit.com/v1/candles/minutes/1?market=KRW-BTC&count=240"
 df = pd.read_json(open_url(url))
 
 fig = plt.figure(figsize=(10, 6))
 
-stock_df = df.copy()
-stock_df = stock_df.rename(columns={'candle_date_time_kst':'Date', 'trade_price':'Close'})
-stock_df = stock_df[['Date', 'Close']]
-stock_df['Date'] = pd.to_datetime(stock_df['Date'], format='%Y-%m-%d %H:%M:%S', errors='raise')
-stock_df.set_index("Date", inplace=True)
+df = df.rename(columns={'candle_date_time_kst':'Date', 'opening_price':'Open', 'high_price':'High', 'low_price':'Low', 'trade_price':'Close', 'candle_acc_trade_volume': 'Volume'})
+df = df[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d %H:%M:%S', errors='raise')
 
-plt.plot(stock_df, color = "red", linestyle = 'solid')
+for i in range(200):
+    if df['Open'][i] < df['Close'][i]:
+        plt.vlines(df['Date'][i], df['Open'][i],df['Close'][i],color='red',linewidth=2, zorder = 2)
+    else:
+        plt.vlines(df['Date'][i], df['Open'][i],df['Close'][i],color='blue',linewidth=2, zorder = 2)
 
+    if df['Open'][i] < df['Close'][i]:
+        plt.vlines(df['Date'][i], df['Low'][i],df['High'][i],color='red', linewidth=1, zorder = 0)
+    else:
+        if df['Open'][i] > df['Close'][i]:
+            plt.vlines(df['Date'][i], df['Low'][i],df['High'][i],color='blue', linewidth=1, zorder = 0)
+        else:
+            plt.vlines(df['Date'][i], df['Low'][i],df['High'][i],color='black', linewidth=1, zorder = 0)
+            plt.plot(df['Date'][i], df['Close'][i],marker='_',color='black', zorder = 2)
+
+plt.tight_layout()
 fig
